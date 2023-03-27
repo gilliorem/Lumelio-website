3be1db20efa34357169b2bd1b8836cfcf44edad0
Remi <remi.gilliot98@gmail.com>
2023-03-27 07:35:05 +0200

    counter-feature

---
+++ site-web/js/counter.js
@@ -0,0 +1,36 @@
+const teamCounterSpan = D.querySelector("#team-count");
+const customerCounterSpan = D.querySelector("#customer-count");
+const reviewCounterSpan = D.querySelector("#review-count");
+
+
+function counter(element, targetValue, timing)
+{
+    let count =0;
+    const options = 
+    {
+        root:null,
+        rootMargin:'-10px',
+        threshold:.5
+    };
+    const obsverver = new IntersectionObserver(function(entries)
+    {
+        if (entries[0].isIntersecting)
+        {
+            const timer = setInterval(()=>
+            {
+                count ++;
+                element.innerText = count;
+                if(count >= targetValue)
+                {
+                    clearInterval(timer);
+                }
+            }, timing);
+            obsverver.disconnect();
+        }
+    }, options);
+    obsverver.observe(element);
+}
+
+counter(teamCounterSpan, 24, 100);
+counter(customerCounterSpan, 231, 40);
+counter(reviewCounterSpan, 179, 50);
