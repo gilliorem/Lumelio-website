b1d18cf7ca1efc4b8d61f2888d76f85a2bf3347e
Remi <remi.gilliot98@gmail.com>
2023-04-05 06:09:44 +0200

    extension-twitch

---
+++ site-web/js/google-review.js
@@ -0,0 +1,21 @@
+
+// const reviewsElement = document.getElementById("reviews");
+
+// fetch('https://maps.googleapis.com/maps/api/place/details/json?placeid=stately-century-382512&fields=name,rating,reviews&key=AIzaSyAs7o2CFtLn6tqSvcrmVJO7CDNw0vj8G_U')
+// .then(response => response.json())
+// .then(data => {
+//     const reviews = data.result.reviews;
+
+//     reviews.forEach(review => {
+//     const reviewElement = document.createElement("div");
+//     reviewElement.innerHTML = `
+//         <div class="review">
+//         <p class="name">${review.author_name}</p>
+//         <p class="rating">${review.rating} stars</p>
+//         <p class="text">${review.text}</p>
+//         </div>
+//     `;
+
+//     reviewsElement.appendChild(reviewElement);
+//     });
+// });
